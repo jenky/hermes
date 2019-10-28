1b94bc5e8a0e7aff99f5906756e94fea1ef07a92
Lynh Tran <jenky.w0w@gmail.com>
2019-10-28 15:20:23 +0700

    Fix doc

--- src/helpers.php
+++ src/helpers.php
@@ -4,9 +4,9 @@ if (! function_exists('guzzle')) {
     /**
      * Get a guzzle client instance.
      *
-     * @param  string  $connection
+     * @param  string|null  $channel
      * @param  array  $options
-     * @return \Jenky\Elastify\Contracts\ConnectionInterface
+     * @return \Jenky\Hermes\Contracts\Hermes
      */
     function guzzle($channel = null, array $options = [])
     {
