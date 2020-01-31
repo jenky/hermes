18fd7d636c6666a82098c0d25166c16fb24f8516
Lynh Tran <jenky.w0w@gmail.com>
2020-01-31 18:10:14 +0700

    Use class name

--- src/helpers.php
+++ src/helpers.php
@@ -1,15 +1,17 @@
 <?php
 
+use Jenky\Hermes\Contracts\Hermes;
+
 if (! function_exists('guzzle')) {
     /**
      * Get a guzzle client instance.
      *
      * @param  string|null  $channel
      * @param  array  $options
-     * @return \Jenky\Hermes\Contracts\Hermes
+     * @return \Jenky\Hermes\Contracts\Hermes|\GuzzleHttp\Client
      */
     function guzzle($channel = null, array $options = [])
     {
-        return $channel ? app('hermes')->channel($channel, $options) : app('hermes');
+        return $channel ? app(Hermes::class)->channel($channel, $options) : app(Hermes::class);
     }
 }
