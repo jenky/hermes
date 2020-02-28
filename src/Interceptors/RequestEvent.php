ec200b2a1aa6f8b65d34eacb0434da5d71a23348
Lynh Tran <jenky.w0w@gmail.com>
2020-02-28 23:33:28 +0700

    Prepare deprecated error message

--- src/Interceptors/RequestEvent.php
+++ src/Interceptors/RequestEvent.php
@@ -4,6 +4,8 @@ namespace Jenky\Hermes\Interceptors;
 
 use Jenky\Hermes\Middleware\RequestEvent as Middleware;
 
+// @trigger_error(sprintf('The "%s" class is deprecated since Hermes 1.3.', RequestEvent::class), E_USER_DEPRECATED);
+
 /**
  * @deprecated Use Jenky\Hermes\Middleware\RequestEvent instead. Will be removed in 2.0
  */
--- src/Interceptors/ResponseHandler.php
+++ src/Interceptors/ResponseHandler.php
@@ -4,6 +4,8 @@ namespace Jenky\Hermes\Interceptors;
 
 use Jenky\Hermes\Middleware\ResponseHandler as Middleware;
 
+// @trigger_error(sprintf('The "%s" class is deprecated since Hermes 1.3.', ResponseHandler::class), E_USER_DEPRECATED);
+
 /**
  * @deprecated Use Jenky\Hermes\Middleware\ResponseHandler instead. Will be removed in 2.0
  */
