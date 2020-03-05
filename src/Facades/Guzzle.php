bba26b5081aed19044c857991102317b696cc8c2
Lynh Tran <jenky.w0w@gmail.com>
2020-03-05 18:29:59 +0700

    Testing

--- src/Facades/Guzzle.php
+++ src/Facades/Guzzle.php
@@ -3,9 +3,25 @@
 namespace Jenky\Hermes\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Jenky\Hermes\Contracts\Hermes;
+use Jenky\Hermes\Testing\Fakes\GuzzleFake;
 
 class Guzzle extends Facade
 {
+    /**
+     * Replace the bound instance with a fake.
+     *
+     * @return \Illuminate\Support\Testing\Fakes\EventFake
+     */
+    public static function fake()
+    {
+        static::swap($fake = new GuzzleFake(
+            static::getFacadeApplication()
+        ));
+
+        return $fake;
+    }
+
     /**
      * Get the registered name of the component.
      *
@@ -13,6 +29,6 @@ class Guzzle extends Facade
      */
     protected static function getFacadeAccessor()
     {
-        return 'hermes';
+        return Hermes::class;
     }
 }
---
+++ src/Testing/Fakes/GuzzleFake.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Jenky\Hermes\Testing\Fakes;
+
+use Jenky\Hermes\GuzzleManager;
+
+class GuzzleFake extends GuzzleManager
+{
+    //
+}
