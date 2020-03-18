9b4c55c9407323bb0367ebda1298e244f4a51626
Lynh Tran <jenky.w0w@gmail.com>
2020-03-18 16:02:34 +0700

    Deprecation notice message

--- src/Interceptors/RequestEvent.php
+++ src/Interceptors/RequestEvent.php
@@ -4,10 +4,10 @@ namespace Jenky\Hermes\Interceptors;
 
 use Jenky\Hermes\Middleware\RequestEvent as Middleware;
 
-// @trigger_error(sprintf('The "%s" class is deprecated since Hermes 1.3.', RequestEvent::class), E_USER_DEPRECATED);
+@trigger_error(sprintf('The "%s" class is deprecated since Hermes 1.3.', RequestEvent::class), E_USER_DEPRECATED);
 
 /**
- * @deprecated Use Jenky\Hermes\Middleware\RequestEvent instead. Will be removed in 2.0
+ * @deprecated 1.3 Use Jenky\Hermes\Middleware\RequestEvent instead. Will be removed in 2.0
  */
 class RequestEvent extends Middleware
 {
--- src/Interceptors/ResponseHandler.php
+++ src/Interceptors/ResponseHandler.php
@@ -4,10 +4,10 @@ namespace Jenky\Hermes\Interceptors;
 
 use Jenky\Hermes\Middleware\ResponseHandler as Middleware;
 
-// @trigger_error(sprintf('The "%s" class is deprecated since Hermes 1.3.', ResponseHandler::class), E_USER_DEPRECATED);
+@trigger_error(sprintf('The "%s" class is deprecated since Hermes 1.3.', ResponseHandler::class), E_USER_DEPRECATED);
 
 /**
- * @deprecated Use Jenky\Hermes\Middleware\ResponseHandler instead. Will be removed in 2.0
+ * @deprecated 1.3 Use Jenky\Hermes\Middleware\ResponseHandler instead. Will be removed in 2.0
  */
 class ResponseHandler extends Middleware
 {
