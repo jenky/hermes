7aea63e412d4a6d3ced3578ea7c007a0dc07e2e3
Lynh Tran <jenky.w0w@gmail.com>
2020-04-08 18:14:23 +0700

    Update facade

--- src/Facades/Guzzle.php
+++ src/Facades/Guzzle.php
@@ -3,7 +3,26 @@
 namespace Jenky\Hermes\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Jenky\Hermes\Contracts\Hermes;
 
+/**
+ * @method static \GuzzleHttp\Client channel(string $name, array $options = [])
+ * @method static \Psr\Http\Message\ResponseInterface get(string|UriInterface $uri, array $options = [])
+ * @method static \Psr\Http\Message\ResponseInterface head(string|UriInterface $uri, array $options = [])
+ * @method static \Psr\Http\Message\ResponseInterface put(string|UriInterface $uri, array $options = [])
+ * @method static \Psr\Http\Message\ResponseInterface post(string|UriInterface $uri, array $options = [])
+ * @method static \Psr\Http\Message\ResponseInterface patch(string|UriInterface $uri, array $options = [])
+ * @method static \Psr\Http\Message\ResponseInterface delete(string|UriInterface $uri, array $options = [])
+ * @method static \GuzzleHttp\Promise\PromiseInterface getAsync(string|UriInterface $uri, array $options = [])
+ * @method static \GuzzleHttp\Promise\PromiseInterface headAsync(string|UriInterface $uri, array $options = [])
+ * @method static \GuzzleHttp\Promise\PromiseInterface putAsync(string|UriInterface $uri, array $options = [])
+ * @method static \GuzzleHttp\Promise\PromiseInterface postAsync(string|UriInterface $uri, array $options = [])
+ * @method static \GuzzleHttp\Promise\PromiseInterface patchAsync(string|UriInterface $uri, array $options = [])
+ * @method static \GuzzleHttp\Promise\PromiseInterface deleteAsync(string|UriInterface $uri, array $options = [])
+ *
+ * @see \Jenky\Hermes\GuzzleManager
+ * @see \GuzzleHttp\Client
+ */
 class Guzzle extends Facade
 {
     /**
@@ -13,6 +32,6 @@ class Guzzle extends Facade
      */
     protected static function getFacadeAccessor()
     {
-        return 'hermes';
+        return Hermes::class;
     }
 }
